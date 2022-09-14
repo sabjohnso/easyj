874b4236dcfdda53a000ccb8e6906a423e6d2b71
Samuel B. Johnson <sabjohnso@yahoo.com>
2022-09-13 21:03:23 -0500

    :sparkles: easyj has an empty test for introspective types

---
+++ easyj/easyj.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+namespace easyj {} // end of namespace easyj
---
+++ easyj_testing/introspective_test.cpp
@@ -0,0 +1,11 @@
+//
+// ... Testing header files
+//
+#include <gtest/gtest.h>
+
+//
+// ... easyj header files
+//
+#include <easyj/easyj.hpp>
+
+namespace easyj::testing {} // end of namespace easyj::testing
